05df10d30e7659528d65efd505d63aaf505fdac1
Viorel <Viorel@noreply.github.com>
2023-12-09 11:16:37 +0200

    Initial code.

---
+++ SampleWindowRestoreWPF/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleWindowRestoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow( )
+        {
+            InitializeComponent( );
+        }
+
+        private void Window_SourceInitialized( object sender, EventArgs e )
+        {
+            TryRestoreWindowPlacement( );
+            RestoreMaximisedState( );
+        }
+
+        private void Window_Closing( object sender, System.ComponentModel.CancelEventArgs e )
+        {
+            SaveWindowPlacement( );
+        }
+
+
+        void SaveWindowPlacement( )
+        {
+            try
+            {
+                Properties.Settings.Default.IsMaximised = WindowState == WindowState.Maximized;
+
+                if( Properties.Settings.Default.IsMaximised )
+                {
+                    Properties.Settings.Default.RestoreBoundsXY = new System.Drawing.Point( (int)RestoreBounds.Location.X, (int)RestoreBounds.Location.Y );
+                    Properties.Settings.Default.RestoreBoundsWH = new System.Drawing.Size( (int)RestoreBounds.Size.Width, (int)RestoreBounds.Size.Height );
+                }
+                else
+                {
+                    Properties.Settings.Default.RestoreBoundsXY = new System.Drawing.Point( (int)Left, (int)Top );
+                    Properties.Settings.Default.RestoreBoundsWH = new System.Drawing.Size( (int)ActualWidth, (int)ActualHeight );
+                }
+
+                Properties.Settings.Default.Save( );
+            }
+            catch( Exception exc )
+            {
+                if( Debugger.IsAttached ) Debugger.Break( );
+                else Debug.Fail( exc.Message, exc.ToString( ) );
+
+                // ignore
+            }
+        }
+
+        [StructLayout( LayoutKind.Sequential )]
+        struct POINT
+        {
+            public Int32 X;
+            public Int32 Y;
+        }
+
+        [DllImport( "user32", SetLastError = true )]
+        static extern IntPtr MonitorFromPoint( POINT pt, Int32 dwFlags );
+
+        const Int32 MONITOR_DEFAULTTONULL = 0;
+
+        static bool IsVisibleOnAnyMonitor( Point px )
+        {
+            POINT p = new( ) { X = (int)px.X, Y = (int)px.Y };
+
+            return MonitorFromPoint( p, MONITOR_DEFAULTTONULL ) != IntPtr.Zero;
+        }
+
+        Point ToPixels( Point p )
+        {
+            Matrix transform = PresentationSource.FromVisual( this ).CompositionTarget.TransformToDevice;
+
+            Point r = transform.Transform( p );
+
+            return r;
+        }
+
+        void TryRestoreWindowPlacement( )
+        {
+            try
+            {
+                Rect r = new( Properties.Settings.Default.RestoreBoundsXY.X, Properties.Settings.Default.RestoreBoundsXY.Y, Properties.Settings.Default.RestoreBoundsWH.Width, Properties.Settings.Default.RestoreBoundsWH.Height );
+
+                if( !r.IsEmpty && r.Width > 0 && r.Height > 0 )
+                {
+                    Point p1, p2;
+                    p1 = r.TopLeft;
+                    p1.Offset( 10, 10 );
+                    p2 = r.TopRight;
+                    p2.Offset( -10, 10 );
+
+                    if( IsVisibleOnAnyMonitor( ToPixels( p1 ) ) || IsVisibleOnAnyMonitor( ToPixels( p2 ) ) )
+                    {
+                        Left = r.Left;
+                        Top = r.Top;
+                        Width = Math.Max( 50, r.Width );
+                        Height = Math.Max( 40, r.Height );
+                    }
+                }
+            }
+            catch( Exception exc )
+            {
+                _ = exc;
+                if( Debugger.IsAttached ) Debugger.Break( );
+
+                // ignore
+            }
+        }
+
+        void RestoreMaximisedState( )
+        {
+            if( Properties.Settings.Default.IsMaximised )
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+    }
+}
